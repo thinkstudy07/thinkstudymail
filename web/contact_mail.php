3fdc4f164948223b1e651444f3e845def86fa219
vinaykanth97 <vinaykennedy@gmail.com>
2023-01-22 16:54:21 +0530

    small changes

--- web/contact_mail.php
+++ web/contact_mail.php
@@ -1,10 +1,3 @@
 <?php
-// the message
-$msg = "First line of text\nSecond line of text";
-
-// use wordwrap() if lines are longer than 70 characters
-$msg = wordwrap($msg,70);
-
-// send email
-mail("athinkstudy@gmail.com","My subject",$msg);
-?>
+mail("athinkstudy@gmail.com","My subject","Testing Mail");
+echo "Hola";
